295067cf58eb5a132cd6217e6071f930f07bbc16
beanjs <502554248@qq.com>
2023-03-07 11:48:22 +0800

    fix

--- database/bark.go
+++ database/bark.go
@@ -1,9 +1,17 @@
 package database
 
-import "github.com/gotify/server/v2/model"
+import (
+	"github.com/gotify/server/v2/model"
+	"github.com/jinzhu/gorm"
+)
 
 func (d *GormDatabase) CreateBark(bark *model.Bark) error {
-	return d.DB.Create(bark).Error
+	e := new(model.Bark)
+	err := d.DB.Where("token = ?", bark.Token).Find(e).Error
+	if err == gorm.ErrRecordNotFound {
+		return d.DB.Create(bark).Error
+	}
+	return nil
 }
 
 func (d *GormDatabase) GetBarks() ([]*model.Bark, error) {
